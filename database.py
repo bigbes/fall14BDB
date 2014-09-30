53d7738dd4cb5d68702f144403758280985cddc7
bigbes <bigbes@gmail.com>
2014-10-01 03:25:20 +0400

    Add first part of checker

---
+++ database.py
@@ -0,0 +1,117 @@
+import os
+import ctypes
+import shutil
+
+class DBException(Exception):
+    pass
+
+class DBC(ctypes.Structure):
+    _fields_ = [
+        ('db_size', ctypes.c_size_t),
+        ('chunk_size', ctypes.c_size_t),
+    ]
+
+class Database(object):
+    def __init__(self, name):
+        self.dll = ctypes.CDLL(name)
+        self.exc = DBException
+        self.format_error = (lambda x: "Error")
+        self.db = self.dll.dbopen('my.db', ctypes.byref(DBC(16*1024*1024, 4096)))
+        if not self.db:
+            raise self.exc("Can't create DB")
+        self.fput = self.dll.db_put
+        self.fget = self.dll.db_get
+        self.fget.restype = ctypes.c_void_p
+        self.fdel = self.dll.db_del
+
+    def put(self, k, v):
+        k_len = len(k)
+        k = ctypes.c_char_p(k)
+        v_len = len(v)
+        v = ctypes.c_char_p(v)
+        rc = self.fput(self.db, k, k_len, v, v_len)
+        if rc == -1:
+            raise self.exc(self.format_error())
+
+    def get(self, k):
+        k_len = len(k)
+        k = ctypes.c_char_p(k)
+        v_len = ctypes.c_int()
+        v = ctypes.c_char_p()
+        rc = self.fget(self.db, k, k_len, ctypes.byref(v), ctypes.byref(v_len))
+        if rc == -1:
+            raise self.exc(self.format_error())
+        return ctypes.string_at(v, v_len.value)
+
+    def delete(self, k):
+        k_len = len(k)
+        k = ctypes.c_char_p(k)
+        rc = self.fdel(self.db, k, k_len)
+        if rc == -1:
+            raise self.exc(self.format_error())
+
+    def cleanup(self):
+        os.remove('my.db')
+
+    def close(self):
+        if self.db:
+            self.dll.db_close(self.db)
+
+    def __del__(self):
+        self.cleanup()
+        self.close()
+
+#############################################
+# Child class example for Sophia (Checker)  #
+#   Repo: http://github.com/pmwkaa/sophia/  #
+# Docs: http://sphia.org/documentation.html #
+#############################################
+class SophiaException(DBException):
+    pass
+
+class Sophia(Database):
+    SPDIR     = 0x00
+
+    SPO_RDWR   = 0x02
+    SPO_CREAT  = 0x04
+
+    def __init__(self, name=None):
+        if name is None:
+            name = './libsophia.so'
+        self.db   = None
+        self.env  = None
+        self.exc  = SophiaException
+        self.format_error = (lambda x: "Error: " + self.dll.sp_error(self.env))
+        self.dll  = ctypes.CDLL('./libsophia.so')
+        self.env  = self.dll.sp_env()
+        if not self.env:
+            raise self.exc('Failed to create ENV')
+        rc = self.dll.sp_ctl(self.env, self.SPDIR, self.SPO_CREAT|self.SPO_RDWR, './db')
+        if rc == -1:
+            raise self.exc(self.format_error())
+        self.db   = self.dll.sp_open(self.env)
+        if not self.db:
+            raise self.exc(self.format_error())
+        self.fput = self.dll.sp_set
+        self.fget = self.dll.sp_get
+        self.fget.restype = ctypes.c_void_p
+        self.fdel = self.dll.sp_delete
+
+    def cleanup(self):
+        shutil.rmtree('db')
+
+    def close(self):
+        if self.db:
+            self.dll.sp_destroy(self.db)
+        if self.env:
+            self.dll.sp_destroy(self.env)
+
+if __name__ == '__main__':
+    a = Sophia()
+    a.put('1', 'hello, mike')
+    assert(a.get('1') == 'hello, mike')
+    a.delete('1')
+    assert(not a.get('1'))
+    a.delete('1')
+    a.cleanup()
+    a.close()
---
+++ gen_workload.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+import yaml
+import random
+
+import itertools
+import string
+
+from ordered_set import OrderedSet
+
+class Distribution(object):
+    pass
+
+class NoneDistribution(Distribution):
+    def __init__(self, key, val=None):
+        self.key = key
+        self.val = val
+        self.pos = 0
+
+    def get(self):
+        if self.pos >= len(self.key):
+            self.pos = 0
+
+        key = self.key[self.pos]
+        val = None if self.val is None else random.choice(self.val)
+        self.pos +=1
+        return (key, val)
+
+class UniformDistribution(Distribution):
+    pass
+
+def randomword(length):
+    return ''.join([random.choice(string.hexdigits) for k in xrange(length)])
+
+class Workload(object):
+    wl_defaults = {
+        'get': 50,
+        'put': 50,
+        'ops': 200,
+        'distrib': 'none',
+        'shuffle': False,
+        'key_size': 20,
+        'min_val_size': 128,
+        'max_val_size': 4096,
+    }
+
+    key_file = 'cities.txt'
+
+    avail_distrib = [
+        'uniform',
+        #'pareto',
+        #'hotspot',
+        #'latest',
+        'none',
+    ]
+
+    def read_config(self, conf):
+        cfg = {} if cfg is None else yaml.load(open(conf, 'r').read())
+        assert(isinstance(cfg, dict))
+        if ('get' in cfg) or ('put' in cfg):
+            self.get = cfg.get('get', 0)
+            self.put = cfg.get('put', 0)
+        else:
+            self.get = self.wl_defaults['get']
+            self.put = self.wl_defaults['put']
+        assert(self.get + self.put == 100)
+        self.ops = cfg.get('ops', self.wl_defaults['ops'])
+        self.distrib = cfg.get('distrib', self.wl_defaults['distrib'])
+        assert(self.distrib in self.avail_distrib)
+        self.shuffle = cfg.get('shuffle', self.wl_defaults['shuffle'])
+        self.key_size = cfg.get('key_size', self.wl_defaults['key_size'])
+        self.min_val_size = cfg.get('min_val_size', self.wl_defaults['min_val_size'])
+        self.max_val_size = cfg.get('max_val_size', self.wl_defaults['max_val_size'])
+        assert(isinstance(self.shuffle, bool))
+
+    def __init__(self, cfg=None):
+        self.read_config(cfg)
+        self.keyset = OrderedSet()
+        self.opset  = []
+        self.opset.extend([0] * long(self.ops*self.put/100))
+        self.opset.extend([1] * long(self.ops*self.get/100))
+        if self.shuffle:
+            random.shuffle(self.opset)
+
+    def generate_insert(self):
+        self.insert_count = xrange()
+
+    def generate(self):
+        self.keys = open(self.key_file, 'r').read().split()
+        if self.shuffle:
+            random.shuffle(self.keys)
+
+        self.keys = [k[:20] for k in self.keys]
+        self.keys = filter((lambda x: len(x) > 10), self.keys)
+        self.keys = self.keys[:500]
+        self.vals = []
+        for k in xrange(len(self.keys)):
+            self.vals.append(
+                    randomword(random.randint(self.min_val_size, self.max_val_size))
+            )
+        put = NoneDistribution(self.keys, self.vals)
+        get = NoneDistribution(self.keyset)
+        for k in self.opset:
+            if k == 0:
+                kv = put.get()
+                self.keyset.append(kv[0])
+                yield ('put', kv[0], kv[1])
+            elif k == 1:
+                kv = get.get()
+                yield ('get', kv[0])
+
+
+if __name__ == '__main__':
+    wl = Workload()
+    with open('1.out', 'w') as f:
+        for k in wl.generate():
+            f.write(' '.join(k)+'\n')
