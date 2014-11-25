f2d567aafccd6f60ebeb3383ff112e203f980f67
bigbes <bigbes@gmail.com>
2014-11-25 19:40:03 +0300

    Fixes

--- test/lib/database.py
+++ test/lib/database.py
@@ -9,10 +9,12 @@ class DBC(ctypes.Structure):
     _fields_ = [
         ('db_size', ctypes.c_size_t),
         ('chunk_size', ctypes.c_size_t),
+        ('mem_size', ctypes.c_size_t),
     ]
 
 class Database(object):
     def_path = './libmydb.so'
+    db_path = 'my.db'
 
     def format_error(self):
         return "Error!"
@@ -25,7 +27,7 @@ class Database(object):
             raise DBException("Can't find SO ad %s", repr(self.so_path))
         self.dll = ctypes.CDLL(self.so_path)
         self.exc = DBException
-        self.db = self.dll.dbcreate('my.db', DBC(16*1024*1024, 4096))
+        self.db = self.dll.dbcreate(self.db_path, DBC(256*1024*1024, 4096, 16*1024))
         if not self.db:
             raise self.exc("Can't create DB")
         self.fput = self.dll.db_put
@@ -66,8 +68,11 @@ class Database(object):
             raise self.exc(self.format_error())
 
     def cleanup(self):
-        if os.path.exists('my.db'):
-            os.remove('my.db')
+        if os.path.exists(self.db_path):
+            if os.path.isdir(self.db_path):
+                shutil.rmtree(self.db_path)
+            else:
+                os.remove(self.db_path)
 
     def close(self):
         if 'db' in dir(self) and self.db:
@@ -108,7 +113,7 @@ class Sophia(Database):
         self.env  = self.dll.sp_env()
         if not self.env:
             raise self.exc('Failed to create ENV')
-        rc = self.dll.sp_ctl(self.env, self.SPDIR, self.SPO_CREAT | self.SPO_RDWR, './db')
+        rc = self.dll.sp_ctl(self.env, self.SPDIR, self.SPO_CREAT | self.SPO_RDWR, self.db_path)
         if rc == -1:
             raise self.exc(self.format_error())
         self.db   = self.dll.sp_open(self.env)
@@ -133,41 +138,8 @@ class Sophia(Database):
             ctypes.c_char_p, ctypes.c_size_t,
         ]
 
-    def cleanup(self):
-        if os.path.exists('./db'):
-            shutil.rmtree('my.db')
-
     def close(self):
         if 'db' in dir(self) and self.db:
             self.dll.sp_destroy(self.db)
         if 'env' in dir(self) and self.env:
             self.dll.sp_destroy(self.env)
-
-# if __name__ == '__main__':
-#     a = Sophia()
-#     a.put('1', 'hello, mike')
-#     assert(a.get('1') == 'hello, mike')
-#     a.put('1', 'hello, mike_bro')
-#     assert(a.get('1') == 'hello, mike_bro')
-#     a.delete('1')
-#     assert(not a.get('1'))
-#     a.delete('1')
-#     a.close()
-#     a.cleanup()
-
-# if __name__ == '__main__':
-#     a = Database()
-#     a.put('1', 'hello, mike')
-#     assert(a.get('1') == 'hello, mike')
-#     print a.get('1')
-#     a.put('1', 'hello, mike_bro')
-#     assert(a.get('1') == 'hello, mike_bro')
-#     print a.get('1')
-#     a.delete('1')
-#     assert(not a.get('1'))
-#     print a.get('1')
-#     a.delete('1')
-#     a.close()
-#     a.cleanup()
-
-
